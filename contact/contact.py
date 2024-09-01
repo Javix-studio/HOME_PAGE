d522bd81b1a01d45dbba6f18e04b19b01a34d429
Durai1759 <rajadurai14008@gmail.com>
2024-09-01 19:29:29 +0530

    1

--- contact/contact.py
+++ contact/contact.py
@@ -3,7 +3,7 @@ import yagmail
 yag = yagmail.SMTP("rajadurai14008@gmail.com", "RAJA1.1.")
 
 yag.send(
-    to="",
+    to="id="email"",
     subject="Subject Here",
     contents="Here is the content of the email"
 )
