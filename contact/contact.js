1cc573638e2df06f9cd6cab60239ff8363a5487c
Durai1759 <rajadurai14008@gmail.com>
2024-09-30 19:26:09 +0530

    1

--- contact/contact.js
+++ contact/contact.js
@@ -1,6 +1,6 @@
 function sendEmail() {
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-    console.log('Email:', email);
-    console.log('Message:', message);
-  }
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+  console.log('Email:', email);
+  console.log('Message:', message);
+}
